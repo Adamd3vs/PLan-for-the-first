842d2362daaafc105870a6e2dc23f1f81e9b7c98
Adam <theesanovmirjalol12@gmail.com>
2025-02-22 21:12:00 +0900

    BBR new

--- server.js
+++ server.js
@@ -51,10 +51,10 @@ app.get("/", function(req, res ){
  
 const server = http.createServer(app);  
 
-let PORT = 3000;  
+let PORT =3000;  
  
 server.listen(PORT, function ( )  {
-    console.log(`ThE server is running succesfully on port ${PORT}`)
+    console.log(`The server is running succesfully on port ${PORT}`)
 }); 
 
 
