b2bb488d689fced0cda2ba7310e6a8c13302329b
Adam <theesanovmirjalol12@gmail.com>
2025-03-04 18:12:49 +0900

    feat: newone

--- train.js
+++ train.js
@@ -1,3 +1,43 @@
+//task C
+class Shop {
+    constructor(non, lagmon, cola) {
+      this.non = non;
+      this.lagmon = lagmon;
+      this.cola = cola;
+    }
+  
+    vaqt() {
+      return new Date().toLocaleTimeString(); // Hozirgi vaqtni chiqaradi
+    }
+  
+    qoldiq() {
+      console.log(
+        `Hozir ${this.vaqt()}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`
+      );
+    }
+  
+    sotish(mahsulot, miqdor) {
+      if (this[mahsulot] >= miqdor) {
+        this[mahsulot] -= miqdor;
+        console.log(`Hozir ${this.vaqt()}da ${miqdor}ta ${mahsulot} sotildi.`);
+      } else {
+        console.log(`Hozir ${this.vaqt()}da yetarli ${mahsulot} yo'q!`);
+      }
+    }
+  
+    qabul(mahsulot, miqdor) {
+      this[mahsulot] += miqdor;
+      console.log(`Hozir ${this.vaqt()}da ${miqdor}ta ${mahsulot} qabul qilindi.`);
+    }
+  }
+  
+  // Foydalanish
+  const shop = new Shop(4, 5, 2);
+  shop.qoldiq();
+  shop.sotish("non", 3);
+  shop.qabul("cola", 4);
+  shop.qoldiq();
+  
 
 
 //task B
